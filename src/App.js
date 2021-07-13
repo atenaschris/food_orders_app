514bb30cce05cd925b3bd86cfe8d65ead78482b2
Christofer cardone <chriscardone69@gmail.com>
2021-07-13 07:36:17 +0200

    Cart component layout completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
+import "./App.css";
+
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <>
-     <Header/>
-     <main>
-      <Meals></Meals>
-     </main>
-     
+      <Header />
+      <main>
+        <Meals></Meals>
+      </main>
     </>
   );
 }
